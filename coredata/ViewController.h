df29c85681975a8977935c92eea9a2c0c9b2121f
alenQingshan <liuqingshan@liuqingshandemac-mini.local>
2016-05-13 13:38:43 +0800

    Initial Commit

---
+++ coredata/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  coredata
+//
+//  Created by 刘青山 on 16/5/13.
+//  Copyright © 2016年 刘青山. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ coredata/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  coredata
+//
+//  Created by 刘青山 on 16/5/13.
+//  Copyright © 2016年 刘青山. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
